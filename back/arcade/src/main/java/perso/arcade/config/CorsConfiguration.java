e80cf73f0263e06bd213d7c1ccf4a1b8ece8655b
bonnier <alobonnier@gmail.com>
2025-08-21 10:51:43 +0200

    feat: Ajout du back

---
+++ back/arcade/src/main/java/perso/arcade/config/CorsConfiguration.java
@@ -0,0 +1,23 @@
+package perso.arcade.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.lang.NonNull;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * Configuration CORS pour autoriser le front
+ */
+@Configuration
+@EnableWebMvc
+public class CorsConfiguration implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(@NonNull CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:5173")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "HEAD")
+                .allowCredentials(true);
+    }
+}
